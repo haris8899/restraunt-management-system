c9917272c2f4b8c071921f8ef6c2db600667f7f9
Hassan-Tanveer321 <hassanrandhawa321@gmail.com>
2021-12-25 23:29:17 +0500

    Setting up nodeJS

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import data from "./data.js"
+const app = express()
+const port=5000
+app.get('/',(req,res)=>{
+    res.send("hello world");
+});
+app.get('/fooditems',(req,res)=>{
+    res.send(data.fooditems);
+});
+app.listen(port, ()=>{
+    console.log(`Server is running at http://localhost:${port}`);
+})
